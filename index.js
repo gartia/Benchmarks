667e8e5a805d878f4f8817c1c0e6d63fe38a0067
Gartia Jenkins <gartia@users.noreply.github.com>
2022-07-29 12:43:19 -0700

    Update index.js
    Added comments to index.js

--- index.js
+++ index.js
@@ -5,22 +5,26 @@ const exec = util.promisify(require("child_process").exec);
 const fs = require("fs");
 
 let primesToTest = [
+  // 0 to x tests of prime numbers
   1000, 10000, 100000, 1000000, 10000000, 100000000, 200000000,
 ];
 let tests = {
+  //Simple object with the name:command syntax
   Node: "node --max-old-space-size=8192 ./tests/javascript.js",
   CPP: '.\\"tests/cpp/x64/Release/cpp.exe"',
   CSHARP: '.\\"tests/csharp/bin/Release/net6.0/csharp.exe"',
   Java: "java -jar ./tests/java/out/artifacts/java_jar/java.jar",
-  //Python: "python ./tests/python.py",
-  //PHP: "php ./tests/php.php",
-  //LUA: "lua ./tests/lua.lua",
+  Python: "python ./tests/python.py",
+  PHP: "php ./tests/php.php",
+  LUA: "lua ./tests/lua.lua",
 };
 const runPrimeTest = async (maxPrime, command) => {
+  //Run the command and return the output of how many prime numbers it found
   const { stdout } = await exec(`${command} ${maxPrime}`);
   return parseInt(stdout);
 };
 const runAll = async () => {
+  //Iterate through tests then iterate through primesToTest, check performance, and then write it to a csv
   await Promise.each(Object.keys(tests), async (lang) => {
     await Promise.each(primesToTest, async (p) => {
       let command = tests[lang];
